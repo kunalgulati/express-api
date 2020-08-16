4f36f17b49f94da4bd6d0b91fae885f9fdbda378
kunal <kunalgulati98@gmail.com>
2020-08-16 16:37:58 -0700

    layout and coloring scheme of the nav bar changed

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -14,14 +14,35 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    width: '100%'
+    width: '100%',
+  },
+  rootBar :{
+    backgroundColor: "#FCA8B2",
+    color: "#ffffff",
+    alignItems: 'center',
+
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
+    textTransform: 'capitalize',
+    paddingLeft: theme.spacing(2),
+    color: '#ffffff'
   },
+  titleLinkedin:{
+    flexGrow: 1,
+    textTransform: 'capitalize',
+    paddingLeft: theme.spacing(2),
+    color: '#0e76a8',
+  },
+  titleGit:{
+    flexGrow: 1,
+    textTransform: 'capitalize',
+    paddingLeft: theme.spacing(2),
+    color: '#000000',
+  }
 }));
 
 export default function ButtonAppBar() {
@@ -48,18 +69,18 @@ export default function ButtonAppBar() {
     <React.Fragment>
 
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.rootBar}>
         <Toolbar>
           <Button color="inherit" onClick={handleHomeClick}>
-            <Typography variant="h6" className={classes.title}>Demo</Typography>
+            <Typography variant="h4" className={classes.title}>{"Demo"}</Typography>
           </Button>
           <Button color="inherit" onClick={handleAboutClick}>
-            <Typography variant="h6" className={classes.title}>Description</Typography>
+            <Typography variant="h4" className={classes.title}>{"Description"}</Typography>
           </Button>
-          <IconButton  target="_blank" href="https://github.com/kunalgulati/express-api" > 
+          <IconButton  target="_blank" href="https://github.com/kunalgulati/express-api" className={classes.titleGit}> 
             <GitHubIcon /> 
           </IconButton>
-          <IconButton  target="_blank" href="https://www.linkedin.com/in/kunal-gulati-a02b1b5b/" > 
+          <IconButton  target="_blank" href="https://www.linkedin.com/in/kunal-gulati-a02b1b5b/" className={classes.titleLinkedin}>
             <LinkedInIcon /> 
           </IconButton>
         </Toolbar>
